d70e1a20101f7018388e92191bd721501b053d36
lchinthakayala <118159853+lchinthakayala@users.noreply.github.com>
2022-11-18 16:08:55 +0530

    Update Program.cs
    Create Fruits Market Program in C#

--- NumberofApples/Program.cs
+++ NumberofApples/Program.cs
@@ -13,26 +13,28 @@ namespace NumberofApples
     {
         static void Main(string[] args)
         {
-            int sum = 0, max = 0;
+            int sum = 0, max = 0,Q=0,NumMar=0;                          // max Indicates The Total Number of Fruits that Martket Have ,, Q and NumMar Indicates just count purpose for Showing  in Console like '1 Query,2 Query' ....and ' Enter the Number of Fruits that  market 1 have ' 
             Console.WriteLine("Enter the Number of shops :");
             int N = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the NUmeber of  Apples have evry shop: ");
             int[] arr1 = new int[N];
             for (int i = 0; i < N; i++)
             {
-                Console.Write("Enter Market ");
+                NumMar++;
+                Console.Write("Enter the number of Fruits that" +NumMar +" Market Have: ");
                 arr1[i] = Convert.ToInt32(Console.ReadLine());
                 max += arr1[i];
             }
-            Console.WriteLine("Enter the Number Of QQ");
+            Console.WriteLine("Enter the Number Of Query");
             int K = Convert.ToInt32(Console.ReadLine());
             int[] arr = new int[K];
             for (int i = 0; i < K; i++)
             {
-                Console.Write(i+"QQ: ");
+                Q++;
+                Console.Write(Q+" Query: ");
                 arr[i] = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("Total Number of Fruit Market:" + max+"\n\n");
+            Console.WriteLine("Total Number of Fruit Market:" + max+"\n\n");  
             for (int z = 0; z < K; z++)
             {
                 for (int i = 1; i <= max; i++)
